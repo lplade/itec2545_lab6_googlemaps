14e53060521e8fb310a523023584b4f780453a29
Larry Lade <lplade@users.noreply.github.com>
2016-10-03 23:54:57 -0500

    Allows re-tries for bad queries

--- src/main/java/name/lplade/Main.java
+++ src/main/java/name/lplade/Main.java
@@ -10,6 +10,7 @@ import com.google.maps.model.LatLng;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import static com.google.maps.GeocodingApi.geocode;
@@ -19,10 +20,10 @@ public class Main {
     static Scanner stringScanner = new Scanner(System.in);
     static Scanner numberScanner = new Scanner(System.in);
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         String key = null;
         //Read key from file
-        try (BufferedReader reader = new BufferedReader(new FileReader("API_KEY"))){
+        try (BufferedReader reader = new BufferedReader(new FileReader("API_KEY"))) {
             key = reader.readLine();
             //System.out.println(key); //just checking...
         } catch (Exception ioe) {
@@ -46,47 +47,82 @@ public class Main {
 
         }*/
 
-        System.out.println("Enter a place name:");
-        String placeToSearchFor = stringScanner.nextLine();
+        //initialize array which is populated inside while loop
+        GeocodingResult[] results;
 
-        GeocodingResult[] results = GeocodingApi.geocode(context, placeToSearchFor).await();
+        //main input loop
+        while (true) {
+
+            System.out.println("Enter a place name: ");
+            String placeToSearchFor = stringScanner.nextLine();
+
+            results = GeocodingApi.geocode(context, placeToSearchFor).await();
+
+            if (results.length >= 1) {
+                System.out.println("I found the following locations:");
+
+                int resultsToDisplay;
+                if (results.length > 5) {
+                    resultsToDisplay = 5;
+                } else {
+                    resultsToDisplay = results.length;
+                }
+
+                for (int i = 0; i < resultsToDisplay; i++) {
+                    System.out.printf("  %d: %s\n", i, results[i].formattedAddress);
+                }
+
+                //TODO some kind of smart handling where any input not 1-5 is assumed to be a new query
+                System.out.print("Is one of these the location you were looking for? (y/n) ");
+                String goodList = stringScanner.nextLine();
+                if (goodList.equals("y")) { //TODO better Y/N input handling
+                    break; //get out of the while loop and continue
+                }
+                System.out.println("Try entering more informationy");
+
+            } else {
+                //No hits
+                System.out.println("Sorry, no results for that place");
+            }
+        }
+
+        System.out.println("Which number do you want?");
+        int choice = intInput();
+        LatLng resultLatLng = results[choice].geometry.location;
+        System.out.println("Latitude, longitude for that is " + resultLatLng); //TODO split and format to 2 decimals
+
+        ElevationResult[] elResults = ElevationApi.getByPoints(context, resultLatLng).await();
 
         if (results.length >= 1) {
-            System.out.println("I found the following locations:");
+            //Get first ElevationResult object
+            ElevationResult resultElevation = elResults[0];
+            //Let's do some rounding
+            System.out.printf("The elevation above sea level is %.2f meters.\n", resultElevation.elevation);
 
-            int resultsToDisplay;
-            if (results.length > 5) {
-                resultsToDisplay = 5;
-            } else {
-                resultsToDisplay = results.length;
-            }
-
-            for (int i=0; i < resultsToDisplay; i++) {
-                System.out.printf("  %d: %s\n", i, results[i].formattedAddress);
-            }
-
-            System.out.println("Which number do you want?");
-            int choice = numberScanner.nextInt(); //TODO validate valid number
-            LatLng resultLatLng = results[choice].geometry.location;
-            System.out.println("Latitude, longitude for that is "+ resultLatLng);
-
-            ElevationResult[] elResults = ElevationApi.getByPoints(context, resultLatLng).await();
-
-            if (results.length >=1) {
-                //Get first ElevationResult object
-                ElevationResult resultElevation = elResults[0];
-                //Let's do some rounding
-                System.out.printf("The elevation above sea level is %.2f meters.\n", resultElevation.elevation);
-
-            }
-
-        } else {
-
-            System.out.println("Sorry, no results for that place");
         }
 
         stringScanner.close();
         numberScanner.close();
 
+    }
+
+    private static int intInput() {
+        //from lab 3 Validation.java
+        while (true) {
+            //Try to read what the user typed as an int.
+            try {
+                // If the input can be read as a int, that int will be returned
+                // This ends the loop, and this method, and control returns to the calling method.
+                return numberScanner.nextInt();
+
+            } // if the input can't be read as an int, then an error will be raised.
+            // For example, if the user enters 'ten' or 1.4 or 123456543454343434, these are not ints, so will cause an error.
+            // That error can be 'caught' by this code, and we can print an error message.
+            // Since we are inside a while loop, then the loop can repeat and ask the user for input again.
+            catch (InputMismatchException ime) {
+                System.out.println("Error - please enter an integer number");
+                numberScanner.next();   //Clear any other characters from the Scanner
+            }
+        }
     }
 }
